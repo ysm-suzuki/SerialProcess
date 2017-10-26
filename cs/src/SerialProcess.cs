b8353aa8d42ac3ee104028b8f13d6ef47e96e9f2
ysm-suzuki <ys443ys443@yahoo.co.jp>
2017-10-26 14:34:21 +0900

    Initiali source commit

---
+++ cs/src/SerialProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class SerialProcess
+{
+    private Queue<System.Action<System.Action>> _processes = new Queue<System.Action<System.Action>>();
+
+    public SerialProcess Add(System.Action<System.Action> process)
+    {
+        _processes.Enqueue(process);
+        return this;
+    }
+
+    public void Flush()
+    {
+        Next();
+    }
+
+    private void Next()
+    {
+        if (_processes.Count == 0)
+            return;
+
+        var process = _processes.Dequeue();
+        process(() =>
+        {
+            Next();
+        });
+    }
+}
